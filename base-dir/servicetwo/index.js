5f995926475ad1ba406ffb583d400b0f29c90fd8
github-main <georgievski.one@gmail.com>
2017-10-09 21:19:19 +0200

    09.10.2017 21:19 commit

--- base-dir/servicetwo/index.js
+++ base-dir/servicetwo/index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const got = require('got');
 
+const disyuntor = require('disyuntor');
+const safeGot = disyuntor.promise(got,{
+	name:'got.request',
+	timeout:'10s',
+	cooldown:'5s',
+	maxFailures:1,
+	onTrip: (err,failures,cooldown) => {
+		console.log(`got request triped because it failed ${failures} times. 
+						Last error was ${err.message}! 
+						There will be no more attempts for ${cooldown}ms`);
+	}
+});
+
 app.get('/:id',(req,res,next) => {
-	return got(`http://basedir_circuitbreaker_1:4000/${req.params.id}`)
+	return safeGot(`http://basedir_circuitbreaker_1:4000/${req.params.id}`)
 			.then(() => {
 				res.sendStatus(200);
 			})
